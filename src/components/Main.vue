fa2273dc729e91ce9330433cd56b66444aa449aa
LAPTOP-ES4T282N\Remco Vaes <remcovaes1995@gmail.com>
2017-09-11 11:55:54 +0200

    should work with disabled lint

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <p>Hello World!</p>
+    <p>testing</p>
+    <p>{{question}}</p>
   </div>
 </template>
+
+<script>
+/* eslint-disable */
+export default {
+  data () {
+    return {
+      question: ""
+    }
+  },
+  created: function () {
+    fetch("https://opentdb.com/api.php?amount=1&type=multiple")
+      .then(response => console.log(response))
+  } 
+  
+}
+
+</script>
